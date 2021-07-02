3268ced222defee90e33d5f4429654844724d9fb
WillianAlbeche <willianalbeche@gmail.com>
2021-07-02 16:12:08 -0300

    merge main to this branch

--- api-tmdb/api-tmdb/API/Movie.swift
+++ api-tmdb/api-tmdb/API/Movie.swift
@@ -2,7 +2,16 @@
 //  Movie.swift
 //  api-tmdb
 //
-//  Created by Luiz Eduardo Mello dos Reis on 02/07/21.
+//  Created by Willian Magnum Albeche & Luiz Eduardo Mello dos Reis  on 02/07/21.
 //
-
 import Foundation
+
+
+struct Movie{
+    let id: Int
+    let title: String
+    let description: String
+    let coverImage: String
+    let rating: Double
+    let genresId: [Int]
+}
