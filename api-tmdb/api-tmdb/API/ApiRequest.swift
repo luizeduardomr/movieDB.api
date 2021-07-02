a4cc486c5df076cb1351be6a7a46a945b7e8d2b8
Luiz Eduardo <luizdudumr@gmail.com>
2021-07-02 00:35:41 -0300

    api base added

---
+++ api-tmdb/api-tmdb/API/ApiRequest.swift
@@ -0,0 +1,17 @@
+//
+//  ApiRequest.swift
+//  api-tmdb
+//
+//  Created by Luiz Eduardo Mello dos Reis on 01/07/21.
+//
+
+import Foundation
+import UIKit
+
+struct ApiRequest{
+    
+    let urlString = "https://api.themoviedb.org/3/movie/550?api_key=06ba203a54577bd8c91c3b4d4b14d0ec"
+    //let url = URL(string: urlString)!
+    
+    //URLSession
+}
---
+++ api-tmdb/api-tmdb/API/Movies.swift
@@ -0,0 +1,19 @@
+//
+//  Movies.swift
+//  api-tmdb
+//
+//  Created by Luiz Eduardo Mello dos Reis on 01/07/21.
+//
+
+import Foundation
+
+
+struct Movies{
+    let id: Int
+    let title: String
+    let description: String
+    let rating: Double
+    let overview: String
+    let coverImage: String
+    
+}
